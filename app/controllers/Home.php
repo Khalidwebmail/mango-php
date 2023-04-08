43c0a46c1ac6a82f59df45f9626977bad50d2a10
khalidwebmail <csewebmail@gmail.com>
2023-04-08 16:36:40 +0100

    correction home controller

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,10 +2,8 @@
 
 class Home extends Controller{
     public function index(){
-        $posts = $this->post_model->getPosts();
         $data = [
             'title'     => 'Welcome',
-            'posts'     => $posts
         ];
         $this->loadView('home/index', $data);
     }
