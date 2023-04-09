071dedad2afe01841bead1fd7ab76995408bed10
khalidwebmail <csewebmail@gmail.com>
2023-04-09 06:19:28 +0100

    no change

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
     require_once 'config/settings.php';
+    require_once 'helper/session_helper.php';
 
     function my_autoloader( $class ) {
         require_once 'lib/' . $class . '.php';
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,8 +2,11 @@
 
 <?php require APPROOT . '/views/inc/header.php'; ?>
     <h1><?php echo $data['title']; ?></h1>
-    <?php 
-        print_r($data['posts'])
-    ?>
+
+    <form action="<?php echo URLROOT . '/home/submitForm'?>" method="post">
+        <input type="hidden" name="_token" value="<?php generateToken() ?>">
+        <input type="text" name="my_name">
+        <input type="submit" name="submit" value="Submit">
+    </form>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
